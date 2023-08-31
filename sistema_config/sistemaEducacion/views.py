c8b77a7bc479f3e62086f3ee2e9a23a1d7da00ac
yiop8900 <veroizaluis@gmail.com>
2023-08-30 20:25:19 -0400

    v-002

--- sistema_config/sistemaEducacion/views.py
+++ sistema_config/sistemaEducacion/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .views import *
+import requests
 
 # Create your views here.
+def base(request):
+    return render(request, 'base.html')
+
+def index(request):
+    return render(request, 'index.html')
