0406ffaf99da788390dddee63c4fbe6e8d0730d0
yiop8900 <veroizaluis@gmail.com>
2023-09-02 22:31:32 -0400

    v-003

--- sistema_config/sistemaEducacion/urls.py
+++ sistema_config/sistemaEducacion/urls.py
@@ -5,4 +5,10 @@ from .views import *
 urlpatterns = [
     path('', base, name='base'),
     path('index/', index, name='index'),
+    path('sistema_Profe/', sistema_Profe, name='sistema_Profe'),
+    path('calificar/', calificar, name='calificar'),    
+    path('planificar/', planificar, name='planificar'),
+    path('clase/', clase, name='clase'), 
+    path('iniciar_sesion/', iniciar_sesion, name='iniciar_sesion'),  
+
 ]
--- sistema_config/sistemaEducacion/views.py
+++ sistema_config/sistemaEducacion/views.py
@@ -7,4 +7,22 @@ def base(request):
     return render(request, 'base.html')
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'is_home': True  # Establece esto en True para la página de inicio
+    }
+    return render(request, 'index.html', context)
+
+def sistema_Profe(reuqest):
+    return render(reuqest, 'Profesor/sistema_Profe.html')
+
+def clase(reuqest):
+    return render(reuqest, 'Profesor/clase.html')
+
+def planificar(reuqest):
+    return render(reuqest, 'Profesor/planificar.html')
+
+def calificar(reuqest):
+    return render(reuqest, 'Profesor/calificar.html')
+
+def iniciar_sesion(reuqest):
+    return render(reuqest, 'iniciar_sesion.html')
